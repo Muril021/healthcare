94cdef133b4bfb69a63ef2dd5fef9339ea574d31
Murilo <murilojesus659@gmail.com>
2024-10-28 17:03:19 -0300

    feat: creating access levels with Laratrust

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => false,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'admin' => [
+            'appointments' => 'c,r,u,d',
+            'doctors' => 'c,r,u,d',
+            'times' => 'c,r,u,d',
+        ],
+        'patient' => [
+            'appointments' => 'c,r,u,d',
+        ],
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete',
+    ],
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             SpecialtySeeder::class,
+            LaratrustSeeder::class,
         ]);
     }
 }
